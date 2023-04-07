4cd73c9a19e68eb1067b762279471878a0d6702b
dieegobs <diegohevant@gmail.com>
2023-04-06 23:37:17 -0300

    Fazendo a parte do menu

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
 export default function Home() {
   return (
     <section>
-      <div className="h-screen w-full background"></div>
+      <div className="h-screen w-full background">
+
+        <div className="flex h-[10%] justify-between p-5">
+          <div>
+            <p className="text-white">IO TEAM</p>
+          </div>
+          <div className="flex items-center gap-x-10 ">
+           
+            <div>
+              <p className="text-white text-sm">LOGIN</p>
+            </div>
+
+            <div>
+              <button className="border border-orange-300 text-white px-3 py-1 text-sm rounded-md">SIGN UP</button>
+            </div>
+
+          </div>
+        </div>
+
+      <div>
+        
+      </div>
+
+      </div>
     </section>
   )
 }
